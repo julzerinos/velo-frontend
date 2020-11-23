881acea4a1153aebc0ab21d542459bb9b36e8ecc
Julian Szachowicz <julian.szachowicz@gmail.com>
2020-11-23 19:03:08 +0100

    VELO-128: updated misplaced bracket & default response code

--- src/plugins/vuex/results/results.js
+++ src/plugins/vuex/results/results.js
@@ -1,5 +1,5 @@
 export const formatResult = function (blame, r) {
-    const status = r['status'] || (r['response'] || {status: 400}['status'])
+    const status = r['status'] || (r['response'] || {status: 503})['status']
 
     const type = status < 299 && status >= 200 ? 'success' : 'error'
 
